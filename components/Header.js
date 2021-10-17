2a2b4e04dea95c311d9d5983ac3093673777c836
Arun <72662617+Arun-1903@users.noreply.github.com>
2021-10-17 11:03:01 +0530

    header modified

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
     <header>
       <div className='container'>
         <Link href='/' passHref>
-          <h2>Abd Blog</h2>
+          <h2>Abd Blogs</h2>
         </Link>
       </div>
     </header>
